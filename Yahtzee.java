ecffcf3f0542693d863bac53d05ffecdd525e767
LoesPK <loeskockelmans@gmail.com>
2019-02-19 16:32:26 +0100

    einde les

--- Yahtzee.java
+++ Yahtzee.java
@@ -1,8 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Yahtzee {
 
 	public static void main(String[] args) {
+		System.out.println("Welkom bij yahtzee, druk op d om te starten");
+		YahtzeeSpel nieuwSpel = new YahtzeeSpel();
+		nieuwSpel.Starten();
+		int worp = nieuwSpel.Werpen();
 		
 	}
 
-}
+}// end class Yahtzee
+
+class YahtzeeSpel{
+	Scanner scanner = new Scanner(System.in);
+	boolean doorgaan = true;
+	
+	void Starten() {
+		while(doorgaan) {
+			String ingave = scanner.nextLine();
+			switch(ingave) {
+				case "d":  System.out.println("Het werkt");
+					break;
+				case "q": System.out.println("Het spel stopt.");
+					break;
+			}
+		}
+	}//end method Starten
+	
+	int Werpen() {
+		Random random = new Random();
+		return random.nextInt(6)+1;
+	}//end method Werpen
+	
+}//end class YahtzeeSpel
