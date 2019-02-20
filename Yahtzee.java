d7b76d97daeec4cfbbb502005d3897ef3ffa7d3f
LoesPK <loeskockelmans@gmail.com>
2019-02-20 09:27:41 +0100

    test ochtend

--- Yahtzee.java
+++ Yahtzee.java
@@ -1,13 +1,13 @@
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Yahtzee {
 
 	public static void main(String[] args) {
-		System.out.println("Welkom bij yahtzee, druk op d om te starten");
+		System.out.println("Welkom bij yahtzee, druk op d om te starten of op q op te stoppen");
 		YahtzeeSpel nieuwSpel = new YahtzeeSpel();
-		nieuwSpel.Starten();
-		int worp = nieuwSpel.Werpen();
+		nieuwSpel.Spelen();
 		
 	}
 
@@ -16,22 +16,47 @@ public class Yahtzee {
 class YahtzeeSpel{
 	Scanner scanner = new Scanner(System.in);
 	boolean doorgaan = true;
+	ArrayList<Integer> dobbelstenen = new ArrayList<Integer>(); 
 	
-	void Starten() {
+	public YahtzeeSpel() {
+		dobbelstenen.add(YahtzeeSpel.Werpen());
+		dobbelstenen.add(YahtzeeSpel.Werpen());
+		dobbelstenen.add(YahtzeeSpel.Werpen());
+		dobbelstenen.add(YahtzeeSpel.Werpen());
+		dobbelstenen.add(YahtzeeSpel.Werpen());
+		dobbelstenen.add(YahtzeeSpel.Werpen());
+	}
+	//onderstaande  method om het spel te starten. wordt aangeroepen in main.
+	void Spelen() {
 		while(doorgaan) {
 			String ingave = scanner.nextLine();
 			switch(ingave) {
-				case "d":  System.out.println("Het werkt jeej");
+				case "d": 
+					
+					for(int i : dobbelstenen) {
+						//YahtzeeSpel.Werpen(); werkt niet
+						System.out.println(i);
+					}
 					break;
-				case "q": System.out.println("Het spel stopt.");
+				case "q": 
+					System.out.println("Het spel stopt. boe");
+					doorgaan = false;
 					break;
 			}
 		}
 	}//end method Starten
 	
-	int Werpen() {
+	// onderstaande is om een random getal tussen 1 en 6 te werpen. deze wordt gereturned.
+	static int Werpen() {
 		Random random = new Random();
 		return random.nextInt(6)+1;
 	}//end method Werpen
 	
 }//end class YahtzeeSpel
+
+class Dobbelsteen{
+
+	void Dobbelstenen() {
+		
+	}
+}// end class Dobbelsteen
